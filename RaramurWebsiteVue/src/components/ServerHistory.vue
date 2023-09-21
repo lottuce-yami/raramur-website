f0f8bf2c4eb651c035f131b6b4d658c1a8aa6ac5
Lotte <lottuce.yami@gmail.com>
2023-09-21 13:52:20 +0200

    Add ServerHistory.vue component

---
+++ RaramurWebsiteVue/src/components/ServerHistory.vue
@@ -0,0 +1,24 @@
+﻿<script setup>
+defineProps(['name', 'version', 'timespan', 'save']);
+</script>
+
+<template>
+  <article>
+    <h2>{{name ?? 'Raramur'}}</h2>
+    <section>
+      <p>{{version}}</p>
+      <p>{{timespan}}</p>
+      <p>
+        <a v-if="save !== undefined" :href="`./saves/${save}.zip`">Сохранение</a>
+        <template v-else>Сохранение недоступно</template>
+      </p>
+    </section>
+    <section>
+      <slot></slot>
+    </section>
+  </article>
+</template>
+
+<style scoped>
+
+</style>
